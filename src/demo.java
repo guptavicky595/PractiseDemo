4ed83dcd786f7663138eea08982fa277294d4da5
guptavicky595 <47935372+guptavicky595@users.noreply.github.com>
2019-08-02 16:19:11 +0600

    added java project
    added java project

---
+++ src/demo.java
@@ -0,0 +1,14 @@
+
+public class demo 
+{
+ int a;
+ String name;
+ 
+ 
+ 
+ public static void main(String[] args) 
+ {
+	System.out.println("Hello World");
+	System.out.println("MyFirstJavaProject");
+ }
+}
